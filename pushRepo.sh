ccf7dc84d2aeb843ac2642059d93b3f13d9f9c81
Dabo Ross <daboross@daboross.net>
2013-12-02 01:02:26 -0800

    Create pushRepo.sh

---
+++ pushRepo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+basedir=`pwd`
+function update {
+    echo $1
+    cd "$basedir/$1"
+    git push git@github.com:daboross/CloudBot.git upstream:$2 -f
+    cd ../
+}
+update Eliralin eliralin
