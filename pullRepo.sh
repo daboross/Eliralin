998d4d89ece26e734f00b50528fc2a3032a5ba6a
Dabo Ross <daboross@daboross.net>
2013-12-03 20:21:50 -0800

    Add posibility of using -f or similar

--- pullRepo.sh
+++ pullRepo.sh
@@ -3,7 +3,7 @@ basedir=`pwd`
 function update {
     echo $1
     cd "$basedir/$1"
-    git pull git@github.com:daboross/CloudBot.git $2:upstream
+    git pull git@github.com:daboross/CloudBot.git $2:upstream $3
     cd ../
 }
-update Eliralin eliralin
+update Eliralin eliralin $1
